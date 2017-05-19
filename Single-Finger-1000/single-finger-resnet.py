e0541c2089ba657d3603486d59c87148d142333d
deepakks1995 <deepakks1995@gmail.com>
2017-05-19 16:44:29 +0530

    test Images html file generated

--- Single-Finger-1000/single-finger-resnet.py
+++ Single-Finger-1000/single-finger-resnet.py
@@ -9,6 +9,7 @@ from sklearn.cross_validation import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
 from keras.utils import np_utils
 import numpy as np
+from PIL import Image
 import os
 import random   
 
@@ -34,7 +35,7 @@ if __name__ == '__main__':
     new_layer2 = Dense(3,activation="softmax")(new_layer1)
     model_1 = Model(input=model.input, output=[new_layer2])
     model_1.compile(optimizer="adam", loss='categorical_crossentropy',metrics=['accuracy'])
-    model_1.summary()
+    # model_1.summary()
     data_folder = "/training-data"
     class_folders = os.listdir(os.getcwd()+data_folder)
     training_data = {}
@@ -84,21 +85,27 @@ if __name__ == '__main__':
 
     model_1.fit(np.array(train_data),
     	np.array(train_labels),
-    		epochs=10,
+    		epochs=1,
     			verbose=1,
     				shuffle=True,
     					batch_size=64,
-    					validation_data=(np.array(test_data), np.array(test_labels)),
-    						class_weight=class_weight)
+    				        validation_data=(np.array(test_data), np.array(test_labels)),
+    					       class_weight=class_weight)
 
     scores = model_1.evaluate(np.array(test_data), np.array(test_labels), verbose=0)
     
+    print (str(model_1.metrics_names) + "\n" + str(scores) + "\n")
+
+    print ("Labels: " )
+    print test_labels[0]
+
     predicted_labels = model_1.predict(np.array(test_data))
     
     predicted_labels = np.argmax(predicted_labels, axis=1)
     
     target_names = ['Fut', 'Lum', 'Sec']
-    
+
+    print predicted_labels    
     conf_matrix = confusion_matrix(np.argmax(test_labels, axis=1), predicted_labels)
     
     printDistinguish("CONFUSION MATRIX:\n"+ str(conf_matrix) )
@@ -110,3 +117,46 @@ if __name__ == '__main__':
     		sum = sum + conf_matrix[i][j]
     	precision_class = conf_matrix[i][i] / sum
     	print ": " + str(precision_class) 
+
+    test_labels_generated = []
+    for list in test_labels:
+        if list[0] == 1:
+            test_labels_generated.append(0)
+        elif list[1] == 1:
+            test_labels_generated.append(1)
+        else:
+            test_labels_generated.append(2)
+
+    test_path = "testPath"
+    f = open('image.html','w')
+    message="""<html>
+    <body>"""
+    for file in range(len(test_labels)):
+        actual = ""
+        predict = ""
+        path= test_path + "/" + str(file)
+        file=int(file)
+        if(test_labels_generated[file]== 0):
+            actual = "fut"
+        elif (test_labels_generated[file]== 1):
+            actual = "lum"
+        elif (test_labels_generated[file]== 2):
+            actual = "sec"
+
+        if(predicted_labels[file]== 0):
+            predict = "fut"
+        elif(predicted_labels[file]== 1):
+            predict = "lum"
+        elif(predicted_labels[file]== 2):
+            predict = "sec"
+        image = Image.fromarray(test_data[file].astype('uint8'))      
+        image.save(path + ".jpeg")  
+        message += """<figure>
+        <p style="float: left; font-size: 9pt; text-align: center; width: 18%; margin-right: 2%; margin-bottom: 0.5em;">
+        <img src=""" + path + ".jpeg" + " " + """alt="Mountain View" style="width: 100%">
+        <caption> true class = """ + actual + "," + " " + """predicted class=""" + predict +  """</caption></p>
+        </figure>"""
+        message += """</body>
+        </html>"""
+    f.write(message)
+    f.close()
